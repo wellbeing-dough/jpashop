074355946ab49b0dcf18afdca102c6b1cffbfad1
wellbeing-dough <kdw990202@naver.com>
2022-08-26 20:36:04 +0900

    연관관계 메소드추가, fetch, cascade 초기값설정

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -17,14 +17,14 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne      //Order와 Member는 다대일 관계
+    @ManyToOne(fetch = FetchType.LAZY)     //Order와 Member는 다대일 관계
     @JoinColumn(name = "member_id")     //매핑을 뭐로할거냐 member_id -> 외래키 이름이 memeber_id가 된다
     private Member member;      //Member와 관계 세팅
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne   //하나의 주문은 하나의 배송
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)   //하나의 주문은 하나의 배송
     @JoinColumn(name = "delivery_id")   //연관관계의 주인은 order에 delivery
     private Delivery delivery;
 
@@ -32,4 +32,21 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; //주문 상태 [ORDER, CANCEL]
+
+    //==연관관계 편의 메서드==// 양방향일때 원자적으로 한코드로 해결
+
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
