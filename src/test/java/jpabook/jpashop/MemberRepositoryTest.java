9fa6a11b50352a0411be526386fc966da1bc3e31
wellbeing-dough <kdw990202@naver.com>
2022-08-29 02:36:19 +0900

    Update MemberRepositoryTest.java

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -15,23 +15,12 @@ import static org.junit.Assert.*;
 @SpringBootTest
 public class MemberRepositoryTest {
 
-    @Autowired MemberRepository memberRepository;
 
     @Test
     @Transactional
     @Rollback
     public void testMember() throws Exception {
-        //given
-        Member member = new Member();
-        member.setUsername("memberA");
-        //when
-        Long savedId = memberRepository.save(member);
-        Member findMember = memberRepository.find(savedId);
 
-        //then
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-        Assertions.assertThat(findMember).isEqualTo(member);
     }
 
 }
