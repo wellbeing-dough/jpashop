28e75f70ce9e9366c32d0c4045ee3d23220514e6
wellbeing-dough <kdw990202@naver.com>
2022-08-29 03:08:22 +0900

    회원 서비스 개발

---
+++ src/main/java/jpabook/jpashop/sevice/MemberService.java
@@ -0,0 +1,48 @@
+package jpabook.jpashop.sevice;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    //회원 가입
+    @Transactional(readOnly = false)
+    public Long join(Member member) {
+
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    //중복회원 검증 로직
+    private void validateDuplicateMember(Member member) {
+        //EXCEPTION
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    //회원 전체 조회
+    @Transactional(readOnly = true)
+    public List<Member> findMembers() {
+    return memberRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+}
