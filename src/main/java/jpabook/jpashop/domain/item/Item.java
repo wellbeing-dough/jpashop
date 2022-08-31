cb85284268c12f0dd687de685870c0583086797c
wellbeing-dough <kdw990202@naver.com>
2022-08-31 11:24:47 +0900

    상품 도메인 개발

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,8 +1,8 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -11,7 +11,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)  //상속관계 매핑이라서 상속관계 전략을 지정해야됨 그걸 부모 클래스에 잡아준다
 @DiscriminatorColumn(name = "dtype")
-@Getter @Setter
+@Getter
 public abstract class Item {
 
     @Id
@@ -25,6 +25,25 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비지니스 로직==//     //재고를 넣고빼는 로직은 stockQuantity 정보를 사용하는데 이게
+    //아이템 entity 가 가지고있기 때문에 핵심 비지니스 로직을 entity 에 직접넣는게 이득
+
+    //재고 수량 증가 로직
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    //재고 수량 감소 로직
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
+    //
 
 
 }
