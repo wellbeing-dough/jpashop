fc70d725c70567faa4c2fb323cf19e065b9cfdb9
wellbeing-dough <kdw990202@naver.com>
2022-09-19 03:28:26 +0900

    상품 주문

---
+++ src/main/java/jpabook/jpashop/controller/orderController.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.sevice.ItemService;
+import jpabook.jpashop.sevice.MemberService;
+import jpabook.jpashop.sevice.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class orderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping ("/order")
+    public String createForm(Model model) {
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+
+        Long orderId = orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+
+
+}
